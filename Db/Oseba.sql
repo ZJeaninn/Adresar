a65200e5bec7b9e9a87319ba64ba00df185006b9
Zanina Vrecl <zanina@nanokom.com>
2019-08-15 13:04:45 +0200

    Tabela Oseba

---
+++ Db/Oseba.sql
@@ -0,0 +1,17 @@
+drop table "Oseba" ;
+
+CREATE TABLE "Oseba"
+
+(    
+   "IdOSeba" INT not null primary key
+        GENERATED ALWAYS AS IDENTITY
+        (START WITH 1, INCREMENT BY 1),   
+   "Priimek" VARCHAR(50),     
+   "Stevilka" VARCHAR(50),
+ 
+   "Posta" INTEGER
+
+);
+
+alter table "Oseba"
+add foreign key ("Posta") references POSTA(IdPosta);
--- src/adresar/Adresar.java
+++ src/adresar/Adresar.java
@@ -5,17 +5,51 @@
  */
 package adresar;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author zanina
  */
 public class Adresar {
-
+// max +1
+    // triger after insert
+    // 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String data = "jdbc:derby://localhost:1527/Oseba";
+        try (
+                Connection conn = DriverManager.getConnection(
+                        data);
+                Statement st = conn.createStatement()) {
+
+                    Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+                    ResultSet rec = st.executeQuery(
+                            "select IdOseba, Priimek, Kraj, Stevilka "
+                            + "from APP.oseba "
+                            + "order by IdOseba");
+                    while (rec.next()) {
+                        System.out.println("IdOseba:\t"
+                                + rec.getString(1));
+                        System.out.println("Priimek:\t" + rec.getString(2));
+                        System.out.println("Kraj:\t" + rec.getString(3));
+                        System.out.println("Stevilka:\t" + rec.getInt(4));
+                        System.out.println();
+                    }
+                    st.close();
+                } catch (SQLException s) {
+                    System.out.println("SQL Error: " + s.toString() + " "
+                            + s.getErrorCode() + " " + s.getSQLState());
+                } catch (Exception e) {
+                    System.out.println("Error: " + e.toString()
+                            + e.getMessage());
+                }
     }
-    
 }
